eef65c6cccd4b864fc0c2a240f46a719d95178c4
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-08-11 11:22:07 -0600

    Implement `earliest_ancestor`

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,33 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    graph = {}
+
+    # Build graph of children's parents
+    for (parent, child) in ancestors:
+        if child in graph:
+            graph[child].add(parent)
+        else:
+            graph[child] = {parent}
+
+    next_verts = [(0, starting_node)]
+    visited = set()
+    oldest = next_verts[0]
+    while len(next_verts) > 0:
+        depth, current = next_verts.pop()
+
+        if current in visited:
+            continue
+        visited.add(current)
+
+        if depth > oldest[0] or depth == oldest[0] and current < oldest[1]:
+            oldest = (depth, current)
+
+        if current not in graph:
+            continue
+
+        for parent in graph[current]:
+            next_verts.append((depth + 1, parent))
+
+    if oldest[1] == starting_node:
+        return -1
+    return oldest[1]
